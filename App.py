23c4a29af0cff89206198083954d4f207e72eb9e
Jeffrey <jefbfitzp33@outlook.com>
2024-01-03 11:49:24 -0700

    boolean imported

--- App.py
+++ App.py
@@ -1,6 +1,31 @@
-from sqlalchemy import Column, Integer, String, create_engine, func
+from sqlalchemy import Column, Integer, String, create_engine, func, Boolean
 from sqlalchemy.orm import Session, declarative_base
 
+Base = declarative_base()
 
-print("Hello world")#
-print("Hello again")
+
+
+print("Your most recent bit of code didn't break the entire program")
+
+class User(Base): 
+    __tablename__ = "Users"
+    id = Column(Integer, primary_key = True )
+    name = Column(String)
+    logged_in = Column(Boolean)  ### Will event be a bool.  does it need to be imported
+
+class Meals(Base):
+    __tablename__="Meals"
+    id = Column(Integer, primary_key = True)
+
+
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///meal_planner.db')
+    Base.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        u1 = User(
+            name = "Leonardo",
+            logged_in = 0
+        )
+        session.add(u1)
+        session.commit()
