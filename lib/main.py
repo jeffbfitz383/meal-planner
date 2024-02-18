3dcc4e5f7f5225f7b5826a63b56a4636550adfc1
Jeffrey <jefbfitzp33@outlook.com>
2024-02-17 18:34:09 -0700

    tables complete

--- lib/main.py
+++ lib/main.py
@@ -1,4 +1,43 @@
-from sqlalchemy import Column,Integer, String, ForeignKey, create_engine
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
 from sqlalchemy.orm import Session, declarative_base, relationship, validates
 
-Base = declarative(Base):
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    user_name = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    tier = Column(Integer, nullable=False)
+    meals = relationship("Meal", back_populates="user")
+
+    ##todo validations
+
+class Meal(Base):
+    __tablename__ = "meals"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    date = Column(Integer, nullable=False)
+    time = Column(Integer, nullable=False)
+    foods = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    users = relationship("User", back_populates="meal")
+
+    #todo validations
+    
+    
+
+class Food(Base):
+    __tablename__ = "foods"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    precent_protein = Column(Integer, nullable=False)
+    percent_calcium =Column(Integer, nullable=False)
+
+    #todo validations
+
+
+##Todo test relationships
+##Todo main Cli
